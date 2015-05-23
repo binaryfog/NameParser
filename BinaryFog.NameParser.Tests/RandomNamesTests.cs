16672883df4853bb51e6a44f1eaf215a6cd4f71f
binaryfog <harapu@gmail.com>
2015-05-23 13:04:47 -0400

    Random names test added

---
+++ BinaryFog.NameParser.Tests/RandomNamesTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomNameGeneratorLibrary;
+
+namespace BinaryFog.NameParser.Tests
+{
+    [TestClass]
+    public class RandomNamesTests
+    {
+        [TestMethod]
+        public void RandomNamesTest()
+        {
+            PersonNameGenerator g = new PersonNameGenerator();
+            string firstName = g.GenerateRandomMaleFirstName();
+            string lastName = g.GenerateRandomLastName();
+
+            string fullName = String.Format("{0} {1}", firstName, lastName);
+            Console.WriteLine(fullName);
+
+            FullNameParser target = new FullNameParser(fullName);
+            target.Parse();
+
+            Assert.AreEqual( firstName, target.FirstName);
+            Assert.AreEqual( lastName, target.LastName);
+            Assert.AreEqual(fullName, target.DisplayName);
+        }
+    }
+}
