6072325ca1efbcfcb96bb049100faa1f9b99d1b2
Cata <Cata@GANDALF>
2016-06-05 16:56:09 -0400

    Updated the application info and publishd to nuget

--- BinaryFog.NameParser/Properties/AssemblyInfo.cs
+++ BinaryFog.NameParser/Properties/AssemblyInfo.cs
@@ -5,13 +5,14 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BinaryFog.NameParser")]
-[assembly: AssemblyDescription("Human name parsing. Parses names using English conventions for persons names.")]
+[assembly: AssemblyDescription("Human name parsing. Parses names using English conventions for persons names. Many thanks for : Tyler Young ")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("BinaryFog.com")]
 [assembly: AssemblyProduct("BinaryFog.NameParser")]
 [assembly: AssemblyCopyright("Copyright © BinaryFog.com 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
