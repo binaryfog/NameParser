3a8c674d45c53d66fe2f7a233ecd5487cf5dc002
Cata <Cata@GANDALF>
2016-11-06 07:05:45 -0500

    Bumped the version number to 1.3.2 and includes hot fix from Matt

--- BinaryFog.NameParser/Properties/AssemblyInfo.cs
+++ BinaryFog.NameParser/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BinaryFog.NameParser")]
-[assembly: AssemblyDescription("Human name parsing. Parses names using English conventions for persons names. Many thanks for : Tyler Young ")]
+[assembly: AssemblyDescription("Human name parsing. Parses names using English conventions for persons names. Many thanks to Tyler Young, Matt Gillette ")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("BinaryFog.com")]
 [assembly: AssemblyProduct("BinaryFog.NameParser")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
