6255a124535fc2a56e2c99e5904871f96499b98b
Catalin Hatmanu <harapu@hotmail.com>
2018-09-10 17:03:25 -0400

    Extra patterns has been added  for handling these cases : "Mr. Jon A. van der Waal Jr." "Mr. Jon A. Waal Jr."

---
+++ BinaryFog.NameParser.Tests/InitialAsMiddleNameTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace BinaryFog.NameParser.Tests {
+
+
+	/// <summary>
+	///This is a test class for FullNameParserTest and is intended
+	///to contain all FullNameParserTest Unit Tests
+	///</summary>
+	public class InitialAsMiddleNameTests
+    {
+		[Fact]
+		public void Parse_MrJonADOTvanderWaalJr() {
+			var fullName = "Mr. Jon A. van der Waal Jr.";
+			var target = new FullNameParser(fullName);
+			target.Parse();
+
+			Assert.Equal("Jon", target.FirstName);
+			Assert.Equal("van der Waal", target.LastName);
+            Assert.Equal("A.", target.MiddleName);
+            Assert.Equal("Mr.", target.Title);
+            Assert.Equal("Jr.", target.Suffix);
+
+        }
+
+        [Fact]
+        public void Parse_MrJonADOTWaalJr()
+        {
+            var fullName = "Mr. Jon A. Waal Jr.";
+            var target = new FullNameParser(fullName);
+            target.Parse();
+
+            Assert.Equal("Jon", target.FirstName);
+            Assert.Equal("Waal", target.LastName);
+            Assert.Equal("A.", target.MiddleName);
+            Assert.Equal("Mr.", target.Title);
+            Assert.Equal("Jr.", target.Suffix);
+
+        }
+
+        [Fact]
+        public void Parse_JonADOTvanderWaalJr()
+        {
+            var fullName = "Jon A. van der Waal Jr.";
+            var target = new FullNameParser(fullName);
+            target.Parse();
+
+            Assert.Equal("Jon", target.FirstName);
+            Assert.Equal("van der Waal", target.LastName);
+            Assert.Equal("A.", target.MiddleName);
+            Assert.Equal("Jr.", target.Suffix);
+
+        }
+
+        [Fact]
+        public void Parse_JonADOTWaalJr()
+        {
+            var fullName = "Jon A. Waal Jr.";
+            var target = new FullNameParser(fullName);
+            target.Parse();
+
+            Assert.Equal("Jon", target.FirstName);
+            Assert.Equal("Waal", target.LastName);
+            Assert.Equal("A.", target.MiddleName);
+            Assert.Equal("Jr.", target.Suffix);
+
+        }
+
+        [Fact]
+        public void Parse_JamesAAdams()
+        {
+            var fullName = "James A Adams";
+            var target = new FullNameParser(fullName);
+            target.Parse();
+
+            Assert.Equal("James", target.FirstName);
+            Assert.Equal("A Adams", target.LastName);
+            
+        }
+
+        [Fact]
+        public void Parse_JamesADOTAdams()
+        {
+            var fullName = "James A. Adams";
+            var target = new FullNameParser(fullName);
+            target.Parse();
+
+            Assert.Equal("James", target.FirstName);
+            Assert.Equal("Adams", target.LastName);
+            Assert.Equal("A.", target.MiddleName);
+        }
+
+
+
+    }
+}
---
+++ BinaryFog.NameParser/Patterns/TitleFirstInitialPrefixedLastSuffixPattern.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+using static BinaryFog.NameParser.RegexNameComponents;
+using static BinaryFog.NameParser.NameComponentSets;
+
+namespace BinaryFog.NameParser.Patterns {
+	[UsedImplicitly]
+	public class TitleFirstInitialPrefixedLastSuffixPattern : IFullNamePattern {
+		private static readonly Regex Rx = new Regex(
+			@"^" + Title + Space + First + Space + Initial + Space + Prefix + Space + Last + Space + Suffix + @"$",
+			CommonPatternRegexOptions);
+
+		public ParsedFullName Parse(string rawName) {
+			var match = Rx.Match(rawName);
+			if (!match.Success) return null;
+            var title = match.Groups["title"].Value;
+            var firstName = match.Groups["first"].Value;
+			var middleName = $"{match.Groups["initial"]}.";
+			var prefix = match.Groups["prefix"].Value;
+			var lastPart = match.Groups["last"].Value;
+			var lastName = $"{prefix} {lastPart}";
+
+			var scoreMod = 0;
+			ModifyScoreExpectedFirstName(ref scoreMod, firstName);
+			ModifyScoreExpectedLastName(ref scoreMod, lastPart);
+
+			var pn = new ParsedFullName {
+                Title = title,
+				FirstName = firstName,
+				MiddleName = middleName,
+				LastName = lastName,
+				Suffix = match.Groups["suffix"].Value,
+				DisplayName = $"{firstName} {middleName} {lastName}",
+				Score = 100
+			};
+			return pn;
+		}
+	}
+}
