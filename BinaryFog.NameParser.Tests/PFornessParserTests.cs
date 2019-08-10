e1f43cb1cb2bfc9d8746efad56cd908516432356
Catalin Hatmanu <harapu@gmail.com>
2019-08-10 14:51:32 -0400

    Last Name, First Name Initial pattern added

---
+++ BinaryFog.NameParser.Tests/PFornessParserTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace BinaryFog.NameParser.Tests {
+
+
+	/// <summary>
+	///This is a test class for FullNameParserTest and is intended
+	///to contain all FullNameParserTest Unit Tests
+	///</summary>
+	public class PFornessParserTests
+    {
+        //Unit test suggested by pforness
+        [Fact]
+        public void Parse_FornessCommaPaulSpaceTDot()
+        {
+            var fullName = "Forness, Paul T.";
+            var target = new FullNameParser(fullName);
+            target.Parse();
+
+            Assert.Equal("Paul", target.FirstName);
+            Assert.Equal("Forness", target.LastName);
+            Assert.Equal("T", target.MiddleName);
+            Assert.Equal("Paul T. Forness", target.DisplayName);
+
+        }
+
+
+
+    }
+}
---
+++ BinaryFog.NameParser/Patterns/LastNameCommaFirstNameInitialPattern.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+using static BinaryFog.NameParser.RegexNameComponents;
+using static BinaryFog.NameParser.NameComponentSets;
+
+namespace BinaryFog.NameParser.Patterns {
+	[UsedImplicitly]
+	public class LastNameCommaFirstNameInitialPattern : IFullNamePattern {
+		private static readonly Regex Rx = new Regex(
+			@"^" + Last + CommaSpace + First + Space + Initial + @"$",
+			CommonPatternRegexOptions);
+
+		public ParsedFullName Parse(string rawName) {
+			var match = Rx.Match(rawName);
+			if (!match.Success) return null;
+			var firstName = match.Groups["first"].Value;
+			var lastName = match.Groups["last"].Value;
+            var middleName = match.Groups["initial"].Value;
+
+            var scoreMod = 0;
+			ModifyScoreExpectedFirstName(ref scoreMod, firstName);
+			ModifyScoreExpectedLastName(ref scoreMod, lastName);
+
+			var pn = new ParsedFullName {
+				FirstName = firstName,
+				LastName = lastName,
+                MiddleName = middleName,
+				DisplayName = $"{firstName} {middleName}. {lastName}",
+				Score = 100 + scoreMod
+			};
+			return pn;
+		}
+	}
+}
